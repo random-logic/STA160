f0437e98fe2fd2043be424173f0a8d7a089cc9e1
Andrew Jowe <andrewjowe@yahoo.com>
2025-06-03 16:43:24 -0700

    clean up

--- preprocessing/pipeline.py
+++ preprocessing/pipeline.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
